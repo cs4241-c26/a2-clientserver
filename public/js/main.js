fd839cd22bbea9f9e83552b032e65b4fc2b99ec7
sstardellis <150394612+sstardellis@users.noreply.github.com>
2025-02-03 14:26:40 -0500

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -7,20 +7,115 @@ const submit = async function( event ) {
     // remains to this day
     event.preventDefault()
 
-    const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
 
-    const response = await fetch( "/submit", {
-        method:'POST',
-        body
-    })
+    // get form data
+    const formData = new FormData(document.querySelector('form'));
 
-    const text = await response.text()
-    console.log( "text:", text )
-}
+    // store form data
+    const data = {};
 
+
+    // fill out form data
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    // call server with post req
+    const response = await fetch('/', {
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
+
+    // server response
+    const result = await response.json();
+
+    // find the table
+    const workoutTable = document.querySelector('#workout-table tbody');
+
+    // get rid of all the rows
+    workoutTable.innerHTML = '';
+
+    // looping through all the fields to add it to the tr element
+    result.forEach((row, index) => {
+        const tr = document.createElement('tr');
+
+        // all our fields
+        // using formIndex to store what row we want to delete
+        tr.innerHTML = `
+            <td>${row['muscle-group']}</td>
+            <td>${row['numexercises']}</td>
+            <td>${row['date']}</td>
+            <td>${row['comments']}</td>
+            <td>${row['workout-intensity']}</td>
+            <td><button class="deleteButton" formIndex="${index}">Delete</button></td>
+        `;
+
+
+        // adding it to the table
+        workoutTable.appendChild(tr);
+    });
+
+    // adding button for deletion, waiting to be pressed
+    document.querySelectorAll('.deleteButton').forEach(button => {
+        button.addEventListener('click', deleteForm);
+    });
+};
+
+// delete form function
+const deleteForm = async (event) => {
+
+    // get the index of the row targeting
+    const index = event.target.getAttribute('formIndex');
+
+    // sending delete request with index to delete which row
+    const response = await fetch('/delete', {
+        method: 'DELETE',
+        body: JSON.stringify({
+            index: index }),
+    });
+
+    // looking for new result of entries
+    const result = await response.json();
+
+    // call function to fix front end
+    updateWorkoutTable(result);
+};
+
+// updating table function after any req to the server
+const updateWorkoutTable = (workoutData) => {
+
+    // find the table and clear existing data
+    const workoutTable = document.querySelector('#workout-table tbody');
+    workoutTable.innerHTML = '';
+
+
+    // for each loop to fill out entries
+    workoutData.forEach((row, index) => {
+        const tr = document.createElement('tr');
+
+        // fill out HTML fields
+        tr.innerHTML = `
+            <td>${row['muscle-group']}</td>
+            <td>${row['numexercises']}</td>
+            <td>${row['date']}</td>
+            <td>${row['comments']}</td>
+            <td>${row['workout-intensity']}</td>
+            <td><button class="deleteButton" formIndex="${index}">Delete</button></td>
+        `;
+
+        // append new row to the table
+        workoutTable.appendChild(tr);
+    });
+
+    // add event listener to each button
+    document.querySelectorAll('.deleteButton').forEach(button => {
+        button.addEventListener('click', deleteForm);
+    });
+};
+
+
+// submit button
 window.onload = function() {
-    const button = document.querySelector("button");
-    button.onclick = submit;
-}
+    const form = document.querySelector('form');
+    form.addEventListener('submit', submit);
+};
